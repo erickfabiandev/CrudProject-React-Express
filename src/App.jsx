07d0f5abe1b764fa4193f10148c3e4e56fd93b43
Erick_Carrasco <erickfabianct@gmail.com>
2023-07-02 07:39:43 -0500

    Form design, and Buttons interaction (ADD, UPDATE, CANCEL)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react'
 import './App.css'
+import FormProduct from './components/FormProduct/FormProduct'
 
 function App() {
 
+  const [productos,setProduct]=useState({});
+  const [idProductActivated,setIdProductActivated]=useState({undefined})
+  const handleAddProduct=(newProduct)=>{ 
+     /** logic to add product - POST*/   
+  }
+  const handleEditProduct=(productEdit)=>{
+    /** logic to add product - POST*/
+  } 
+  const hanldeDeleteProduct=()=>{
+     /**logic to delete product */
+  } 
+  const hanldeSearchProduct=(id)=>{
+     /** logic to search product */
+  }
+  const productActivated=(id)=>{
+    setIdProductActivated(id);
+  } 
   
   return (
-    <>
-      <h1>CRUD PROJECT </h1>     
-       
+    <>      
+      <div className='vistareporte'>vista Reporte</div> 
+      <FormProduct idProductEdit={1}/>  
+
     </>
   )
 }
