f63a49187b0e83a1b903bca1fce69b1437c30e01
viamazoba <viamazoba@unal.edu.co>
2023-07-04 18:45:02 -0500

    Modificación tabla

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,25 +6,28 @@ export const Main = ()=> {
   
     return (
       <>
-      <div className='text-container'>
-        <h1>Product List</h1>
-        <button>Add</button>
-      </div>
-      <div className='table-container'>
-        <table className="table">
-            <thead>
-                <tr>
-                <th scope="col">PRODUCT NAME</th>
-                <th scope="col">COLOR</th>
-                <th scope="col">CATEGORY</th>
-                <th scope="col">PRICE</th>
-                <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <Row></Row>
-            </tbody>
-        </table> 
+      <div className='container__main'>
+        <div className='text-container'>
+          <h1>Product List</h1>
+          <button>Add</button>
+        </div>
+        <div className='table-container'>
+          <table className="table">
+              <thead>
+                  <tr>
+                  <th scope="col">PRODUCT NAME</th>
+                  <th scope="col">COLOR</th>
+                  <th scope="col">CATEGORY</th>
+                  <th scope="col">PRICE</th>
+                  <th scope="col"></th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <Row></Row>
+              </tbody>
+          </table> 
+
+        </div>
 
       </div>
          
